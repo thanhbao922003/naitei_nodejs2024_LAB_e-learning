7e4364b60b8ebe758113fc5fb4b6e55a94184c69
huongnt-2545 <49477980+huongnt-2545@users.noreply.github.com>
2024-10-01 10:00:48 +0700

    Design Static Page And i18next

---
+++ src/public/js/language-switcher.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+  const params = new URLSearchParams(window.location.search);
+  const currentLang = params.get("lang") || "en";
+  $(".item-lang").each(function () {
+    $(this).on("click", function (e) {
+      e.preventDefault();
+      const newLang = $(this).data("lang");
+      const newUrl = new URL(window.location);
+      newUrl.searchParams.set("lang", newLang);
+      window.location.href = newUrl;
+    });
+    if ($(this).data("lang") === currentLang) {
+      $(this).addClass("active-lang");
+    }
+  });
+});
