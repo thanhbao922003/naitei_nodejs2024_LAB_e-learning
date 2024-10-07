55138c49a5bfbf498930a6002e2edcb4aaeb7469
thanhbao922003 <thanhbao2003@gmail.com>
2024-10-07 15:31:46 +0700

    Initialize migration

--- src/routes/course.routes.ts
+++ src/routes/course.routes.ts
@@ -3,10 +3,10 @@ import * as courseController from '../controller/course.controller';
 
 const router: Router = Router();
 
-router.get('/', courseController.courseList);
 
-router.get('/:id', courseController.courseDetails);
+router.get('/', courseController.courseShowGet);  
 
 router.post('/filter', courseController.filterAndSort)
+
 
 export default router;
